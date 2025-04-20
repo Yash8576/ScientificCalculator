6679292d7f43d9c93a0fadb7926f8a47fc2caed1
Dammalapati Yaswanth Ravi Teja <102867552+Yash8576@users.noreply.github.com>
2025-04-19 21:41:54 -0400

    Firebase setup done

--- app/src/main/java/edu/uga/cs/scientificcalculator/activity/MainActivity.java
+++ app/src/main/java/edu/uga/cs/scientificcalculator/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package edu.uga.cs.scientificcalculator;
+package edu.uga.cs.scientificcalculator.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import edu.uga.cs.scientificcalculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
